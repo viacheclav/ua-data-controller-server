fc8433c5419a1437b59d265b6b25ff89ff7f57ed
slavik <wayonmail@gmail.com>
2017-05-28 01:04:02 +0200

    modify code for deploying on java ee application server

--- src/main/java/com/uadatacollector/uadatacollector/UadatacollectorApplication.java
+++ src/main/java/com/uadatacollector/uadatacollector/UadatacollectorApplication.java
@@ -3,13 +3,20 @@ package com.uadatacollector.uadatacollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class UadatacollectorApplication {
+public class UadatacollectorApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UadatacollectorApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UadatacollectorApplication.class, args);
